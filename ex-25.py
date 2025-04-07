bbd5480a895b1981b61592fa4aab342b2cfd74ac
sm0307-2008 <samgabraf@gmail.com>
2025-04-07 19:29:06 -0300

    Create ex-25.py

---
+++ ex-25.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um número: '))
+while n1>=0 or n2>=0:
+    print(n1+n2)
+    n1 = float(input('Digite um número: '))
+    n2 = float(input('Digite um número: '))
+print('encerrado')
