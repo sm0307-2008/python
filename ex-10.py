e1e3c6ded8134d76cbcfc4ae43881401fad71bb7
sm0307-2008 <samgabraf@gmail.com>
2025-04-07 20:23:53 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,10 @@
+print('Média')
+
+n1 = float(input('Digite a primeira nota: '))
+n2= float(input('Digite a primeira nota'))
+n3= float(input('Digite a primeira nota'))
+n4= float(input('Digite a primeira nota'))
+
+media = (n1+n2+n3+n4)/4
+
+print('Sua média é:', media)
